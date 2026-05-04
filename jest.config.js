8671e7f6e02c39ca9d9e0beddd249b3856c7b773
ana-cerqueiraf <ana.ferreira@sofist.co>
2026-05-04 15:24:28 -0300

    feat: adiciona arquivo package.lock.json e ajusta comentários de outros arquivos

--- jest.config.js
+++ jest.config.js
@@ -2,24 +2,8 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testTimeout: 30000, 
 
-  // 1. O testEnvironment DEVE ser o allure-jest/node
   testEnvironment: "allure-jest/node",
-
-  // 2. Opções adicionais para o Allure (Opcional, mas recomendado)
   testEnvironmentOptions: {
-    resultsDir: "./allure-results", // Pasta onde os dados brutos serão salvos (padrão é allure-results)
+    resultsDir: "./allure-results",
   },
-
-  // 3. Mantenha o reporter 'default' se você quiser ver a saída no terminal também
-  /* reporters: [
-    'default',
-    [ 'jest-html-reporters', {
-        publicPath: './reports',
-        filename: 'trpAPI-test-report.html',
-        expand: true,
-        openReport: true,
-        pageTitle: '+Transportes Transportador [BFF]'
-      }
-    ]
-  ] */
 };
